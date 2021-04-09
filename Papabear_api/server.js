a4f82558260ac7cbf1a3416233b3193b79771c3b
MarQuez LeDay <quezleday@MarQuezs-MBP.attlocal.net>
2021-04-09 14:32:50 -0400

    Successfully connected mongoose

--- Papabear_api/server.js
+++ Papabear_api/server.js
@@ -3,6 +3,15 @@ const mongoose = require('mongoose')
 
 const app = express()
 const PORT = 3000
+const DBNAME = 'papabear'
+
+//DATABASE
+mongoose.connect(`mongodb://localhost:27017/${DBNAME}`, { useNewUrlParser: true })
+
+mongoose.connection.once('open', () => {
+    console.log('connected to mongoose...')
+})
+
 
 
 // LISTENER
