86535c07aa6238b77137169037d97810dc195f7f
MarQuez LeDay <quezleday@MarQuezs-MBP.attlocal.net>
2021-04-09 14:35:53 -0400

    Added CORS to server

--- Papabear_api/server.js
+++ Papabear_api/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
 const PORT = 3000
@@ -11,6 +12,21 @@ mongoose.connect(`mongodb://localhost:27017/${DBNAME}`, { useNewUrlParser: true 
 mongoose.connection.once('open', () => {
     console.log('connected to mongoose...')
 })
+
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
 
 
 
